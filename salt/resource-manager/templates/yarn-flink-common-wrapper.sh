fd2d618c3d7c36cfa9e82fc82b913bc25a133e79
Hitesh Tiwari <hitesh.tiwari@maplelabs.com>
2018-03-07 15:02:10 +0530

    Wrapper to use the yarn queue policies by user for flink applications

--- salt/resource-manager/templates/yarn-flink-common-wrapper.sh
+++ salt/resource-manager/templates/yarn-flink-common-wrapper.sh
@@ -2,7 +2,7 @@
 # Find which script to call
 CALLER_DIR="$(cd "`dirname "$0"`"; pwd)"
 CLI=`basename "$0"`
-EXECUTABLE="/opt/pnda/flink-1.4.0/bin/flink"
+EXECUTABLE="/opt/pnda/flink-1.4.0/bin/$CLI"
 
 # Set the queue in the argument list
 
@@ -20,7 +20,11 @@ do
     --yarnqueue)
         REQUEST="$2"
         shift 2
-      ;; 
+      ;;
+	--qu)
+        REQUEST="$2"
+        shift 2
+      ;;
     *)
       KEEP+=("$1")
       shift
@@ -29,8 +33,7 @@ do
 done
 
 # define the appropriate queue according to the policy
-echo "REQUEST----- $REQUEST"
-QUEUE=`/opt/pnda/rm-wrapper/yarn-policy.sh $REQUEST`
+QUEUE=`{{ policy_file_link }} $REQUEST`
 EXIT_CODE=$?
 if [ $EXIT_CODE -ne 0 ]; then
   echo "Warning: yarn-policy returned exit $EXIT_CODE:  $QUEUE" >> /var/log/pnda/wrapper.log
@@ -39,9 +42,15 @@ fi
 
 # assemble the arguments and call the function
 if [ "X$QUEUE" != "X" ]; then
-    echo "${KEEP[@]:0:3} -yqu $QUEUE ${KEEP[@]:3}"
+    if [ $CLI != "start-scala-shell.sh" ]; then
+       echo "executing $EXECUTABLE ${KEEP[@]:0:3} -yqu $QUEUE ${KEEP[@]:3}"
+       exec "$EXECUTABLE" `echo "${KEEP[@]:0:3} -yqu $QUEUE ${KEEP[@]:3}"`
+    else
+       echo "executing $EXECUTABLE ${KEEP[@]:0:1} -qu $QUEUE ${KEEP[@]:1}"
+       exec "$EXECUTABLE" `echo "${KEEP[@]:0:1} -qu $QUEUE ${KEEP[@]:1}"`
+    fi
+    echo "executing $EXECUTABLE ${KEEP[@]:0:3} -yqu $QUEUE ${KEEP[@]:3}"
     exec "$EXECUTABLE" `echo "${KEEP[@]:0:3} -yqu $QUEUE ${KEEP[@]:3}"`
-    # exec "$EXECUTABLE" `echo "${KEEP[0]} ${KEEP[1]} ${KEEP[2]} -yqu $QUEUE ${KEEP[@]:3}"`
 else
     echo "$REQUEST queue is not exist or user is not authorized to use $REQUEST queue"
     exit 1    
